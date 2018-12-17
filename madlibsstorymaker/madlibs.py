d867b5cbdc0d8588565fdfe965ec001c70925fb3
Madeline Reeves <missy11217@gmail.com>
2018-12-17 13:56:15 +0000

    Madlibs done

---
+++ madlibsstorymaker/madlibs.py
@@ -0,0 +1,19 @@
+print("We will play Madlibs! Enter words that fit the catagory!")
+a = (input("name: ").capitalize())
+b = input("adjective: ")
+c = input("place: ")
+d = input("food: ")
+e = input("drink: ")
+f = (input("name: ").capitalize())
+g = input("adverb: ")
+h = input("verb: ")
+
+if b[0] in "aeiou":
+    x = "an"
+else :
+    x = "a"
+
+madlib = f'''{a} is having {x} {b} party in {c}. They are serving
+ {d}s and {e}. {f} was so {g} to be invited, that they {h} for days.'''
+ 
+print(madlib)
