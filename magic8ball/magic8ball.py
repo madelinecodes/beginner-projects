e585e94032ebe45a6089a0dd15b2537bedd4cb4a
Madeline Reeves <missy11217@gmail.com>
2018-12-17 11:32:54 +0000

    magic 8 ball done

---
+++ magic8ball/magic8ball.py
@@ -0,0 +1,35 @@
+import time
+import random
+
+going = True
+
+while going:
+    question = input("Ask the 8 ball a question or enter Q to quit:  ")
+    answers = random.randint(1,8)
+
+    if question.lower() == "q":
+        exit()
+  
+    elif answers == 1:
+        print ("It is certain")
+    
+    elif answers == 2:
+        print ("Outlook good")
+    
+    elif answers == 3:
+        print ("You may rely on it")
+    
+    elif answers == 4:
+        print ("Ask again later")
+    
+    elif answers == 5:
+        print ("Concentrate and ask again")
+    
+    elif answers == 6:
+        print ("Reply hazy, try again")
+    
+    elif answers == 7:
+        print ("My reply is no")
+    
+    elif answers == 8:
+        print ("My sources say no")
