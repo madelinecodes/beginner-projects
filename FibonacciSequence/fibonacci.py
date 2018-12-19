4d5476c5b258ff84801f597c334e69d1cf076347
Madeline Reeves <missy11217@gmail.com>
2018-12-19 14:34:44 +0000

    Fibonacci Sequence done

---
+++ FibonacciSequence/fibonacci.py
@@ -0,0 +1,37 @@
+count = 0
+n0 = 0
+n1 = 1
+fib = ["0", "1", ]
+nth = int(input("Which placement would you like access to?  "))
+# accessing the last digit of input for ordinal numbers
+last = int((str(nth))[len(str(nth)) -1])
+
+if nth == 0:
+    v = 0
+elif nth == 1:
+    v = 1
+else:
+    while count < nth - 1:
+        n = n0 + n1
+        n0 = n1
+        n1 = n
+        f = str(n)
+        count += 1
+        fib.append(f)
+
+    v = str(f)
+
+# the below is just so the print statement looks nice
+if last == 1:
+    nth = str(nth)+"st"
+elif last == 2:
+    if nth == 12:
+        nth = str(nth)+"th"
+    else:
+        nth = str(nth)+"nd"
+elif last == 3:
+    nth = str(nth)+"rd"
+else:
+    nth = str(nth)+"th"
+
+print("The {nth} number has a value of {v}".format(nth=nth, v=v))
