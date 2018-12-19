adb8f9a7ead85e8ae88f51a3551927a73a7335eb
Madeline Reeves <missy11217@gmail.com>
2018-12-19 14:57:34 +0000

    Fibonacci Sequence updated

--- FibonacciSequence/fibonacci.py
+++ FibonacciSequence/fibonacci.py
@@ -2,26 +2,33 @@ count = 0
 n0 = 0
 n1 = 1
 fib = ["0", "1", ]
-nth = int(input("Which placement would you like access to?  "))
-# accessing the last digit of input for ordinal numbers
-last = int((str(nth))[len(str(nth)) -1])
+nth = (input("Which placement would you like access to?  "))
 
-if nth == 0:
-    v = 0
-elif nth == 1:
-    v = 1
-else:
-    while count < nth - 1:
-        n = n0 + n1
-        n0 = n1
-        n1 = n
-        f = str(n)
-        count += 1
-        fib.append(f)
+try:
+    nth = int(nth)
+    if nth >= 0:
+        if nth == 0:
+            v = 0
+        elif nth == 1:
+            v = 1
+        else:
+            while count < nth - 1:
+                n = n0 + n1
+                n0 = n1
+                n1 = n
+                f = str(n)
+                count += 1
+                fib.append(f)
 
-    v = str(f)
-
+            v = str(f)
+    else:
+        print("Sorry, thats not valid. The placement must be positive.")
+        exit()
+except ValueError:
+    print("Please only enter numbers!")
+    exit()
 # the below is just so the print statement looks nice
+last = int((str(nth))[len(str(nth)) - 1])
 if last == 1:
     nth = str(nth)+"st"
 elif last == 2:
@@ -34,4 +41,4 @@ elif last == 3:
 else:
     nth = str(nth)+"th"
 
-print("The {nth} number has a value of {v}".format(nth=nth, v=v))
+print("The {nth} number has a value of {v}!".format(nth=nth, v=v))
