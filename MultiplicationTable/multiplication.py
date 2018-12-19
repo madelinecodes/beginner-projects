0b6d4e8988da1c08deb3e51135bc63145e94e65c
Madeline Reeves <missy11217@gmail.com>
2018-12-19 12:12:21 +0000

    multiplication table with updated bits

--- MultiplicationTable/multiplication.py
+++ MultiplicationTable/multiplication.py
@@ -1,4 +1,5 @@
-max = range(1, 10)
+x = (int(input("How high would you like the table to go? Enter here:  "))) + 1
+max = range(1, x)
 spaces = len(str(max[-1] * max[-1])) + 1
 
 for i in max:
@@ -6,5 +7,8 @@ for i in max:
     for j in max:
         num = str(i * j)
         padding = (spaces - len(num)) * ' '
-        line += num + padding
+        line += num + padding + "| "
+        hor = (len(line)-1) * '-'
+
     print(line)
+    print (hor)
