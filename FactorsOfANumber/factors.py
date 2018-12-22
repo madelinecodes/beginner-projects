51deec8f860ebc6287576080baca33407e59d94c
Madeline Reeves <missy11217@gmail.com>
2018-12-22 01:06:42 +0000

    factors of a number done

--- FactorsOfANumber/factors.py
+++ FactorsOfANumber/factors.py
@@ -1,17 +1,31 @@
 x = int(input("Find all factors. Enter your number here:  "))
+def is_prime(i):
+    is_prime = False
+    for j in range(2, i):
+        if (i % j) == 0:
+            is_prime = False
+        else:
+            is_prime = True
+    return is_prime
 
 def factor(x):
     facts=[]
     if x > 0:
         for i in range(1, x + 1):
             if (x % i) == 0:
-                facts.append(i)
+                if is_prime(i) is True or i == 2:
+                    i = str(i)
+                    facts.append(i + '*')
+                else:
+                    facts.append(i)
         return facts
     elif x < 0:
         for i in range (x-1, 0):
             if x % i == 0:
-                facts.append(i)
+                    facts.append(i)
         return facts
     elif x == 0:
         print("All numbers are factors of Zero my friend.")
 print (', '.join(map(str, factor(x))))
+print('*indicates a prime number')
+
--- WhatsMyNumber/number.py
+++ WhatsMyNumber/number.py
@@ -1,7 +1,7 @@
 import math
 num = 10
 def is_prime(num):
-    for i in range(2, num - 1):
+    for i in range(2, num):
         if (num % i) == 0:
             is_prime = False
             break
