f8ea2ae6107eb7e8b2c92656269428230cb9cf3b
Madeline Reeves <missy11217@gmail.com>
2019-01-11 20:02:47 +0000

    Added subgoals to game

--- TurnBasedPokemon/pokemongame.py
+++ TurnBasedPokemon/pokemongame.py
@@ -15,51 +15,63 @@ while comp_health and player_health > 0:
         if move not in [1, 2, 3]:
             print('I didnt recognize that number.')
         elif move is 1:
-            number = random.randint(18, 25)
-            comp_health -= number
-            comp_health = max(comp_health, 0)
+            number = random.randint(18, 25) # set here instead of inline below so that number can be called in print statement 
+            comp_health -= number 
+            comp_health = max(comp_health, 0)   # makes sure that health never displays below 0
+            # affected, dmg_heal, and move_name are only for the sake of the printed message
             affected = 'Computer'
             dmg_heal = 'damaged'
+            move_name = 'STRIKE'
         elif move is 2:
             number = random.randint(10, 35)
             comp_health -= number
             comp_health = max(comp_health, 0)
             affected = 'Computer'
             dmg_heal = 'damaged'
+            move_name = 'FURY'
         elif move is 3:
             number = random.randint(18, 25)
             player_health += number
-            player_health = min(player_health, 100)
+            player_health = min(player_health, 100) # makes sure that health doesnt go above 100
             affected = 'Player'
             dmg_heal = 'healed'
-        turn = 1 - turn
+            move_name = 'RECOVERY'
+        turn = 1 - turn # toggle turn between 0 and 1
     else:
         move = random.randint(1, 3)
-        time.sleep(1)
+        time.sleep(1) # 'loading time'
+        if comp_health < 35:     # increase probability of choosing 3 if comp_health is under 35 points
+            choices = [1, 2] + [3] * 2
+            move = random.choice(choices)
         if move is 1:
             number = random.randint(18, 25)
             player_health -= number
             player_health = max(player_health, 0)
             affected = 'Player'
             dmg_heal = 'damaged'
+            move_name = 'STRIKE'
         elif move is 2:
             number = random.randint(10, 35)
             player_health -= number
             player_health = max(player_health, 0)
             affected = 'Player'
             dmg_heal = 'damaged'
+            move_name = 'FURY'
         elif move is 3:
             number = random.randint(18, 25)
             comp_health += number
             comp_health = min(comp_health, 100)
             affected = 'Computer'
             dmg_heal = 'healed'
+            move_name = 'RECOVERY'
         turn = 1 - turn
     print('*' * 36)
+    print('\n!!!{}!!!\n'.format(move_name))
     print('''{affected} has been {dmg_heal} by {number} points. \n 
         Player:{player_health}/Computer:{comp_health}
         '''.format(affected=affected, dmg_heal=dmg_heal, number=number, player_health=player_health, comp_health=comp_health))
     print('*' * 36)
+# if either player has reached zero, end the game
 if comp_health == 0 or player_health == 0:
     print('GAME OVER! Player:{player_health}/Computer:{comp_health}'.format(
         player_health=player_health, comp_health=comp_health))
